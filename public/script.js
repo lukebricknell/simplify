6c9c47d555973a4ce360ea9d1dab514240cf91b5
lukebricknell <lukecbricknell@gmail.com>
2023-01-12 22:36:30 +0200

    first commit

---
+++ public/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const phoneMenu = document.querySelector("#phone-menu");
+const menuButton = document.querySelector("#menu-btn");
+
+menuButton.addEventListener("click", function () {
+  if (phoneMenu.classList.contains("hidden")) {
+    phoneMenu.classList.remove("hidden");
+    phoneMenu.classList.add("flex");
+  } else if (!phoneMenu.classList.contains("hidden")) {
+    phoneMenu.classList.remove("flex");
+    phoneMenu.classList.add("hidden");
+  }
+});
+
+document.addEventListener("keydown", function (e) {
+  if (!phoneMenu.classList.contains("hidden") && e.key == "Escape") {
+    phoneMenu.classList.remove("flex");
+    phoneMenu.classList.add("hidden");
+  }
+});
